ef060fee88cfcfbcef89ce8e6728dcf9073b02e0
Serena Hasler <srhasler@yahoo.com>
2020-04-06 16:12:45 -0500

    Add example includes

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const soBored = ["cooking", "reading", "walking", "sewing", "sleeping"];
 
-const spliced = soBored.splice(2, 0, "bicycling");
-
-console.log(spliced); //what was removed?
-console.log(soBored);
-
-const spliced2 = soBored.splice(2, 1, "yoga");
-console.log(spliced2); //what was removed?
-console.log(soBored);
+const present = soBored.includes("walking");
+console.log(present);
