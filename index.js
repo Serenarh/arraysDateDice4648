bc57e5e7d4fb502e5ca06d820db464534af1a4e8
Serena Hasler <srhasler@yahoo.com>
2020-04-06 20:35:47 -0500

    Use 'Date.now()' to get 'now' in ms.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function calculateAge(dob) {
   // TODO: Use 'Date.parse()' to get dob in ms.
   const birthDate = Date.parse(dob);
   const firstDate = ms2Years(birthDate);
+
+  // TODO: Use 'Date.now()' to get 'now' in ms.
+  const today = Date.now();
+  const today2 = ms2Years(today);
 }
 
 function ms2Years(ms) {
